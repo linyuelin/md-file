2c9bda9a1e98ae8f6a852044541bd401dfae10e3
linyuelin <linyuelin1997@gmail.com>
2024-01-07 17:08:33 +0900

    フード詳細展開機能、ボディの簡潔化

---
+++ FoodPicker/FoodPicker/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  EXtensions.swift
+//  FoodPicker
+//
+//  Created by dreaMTank on 2024/01/06.
+//
+
+import Foundation
---
+++ FoodPicker/FoodPicker/SuffixWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  SuffixWrapper.swift
+//  FoodPicker
+//
+//  Created by dreaMTank on 2024/01/07.
+//
+
+import Foundation
